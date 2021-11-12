fdc611df6fc9876b14c1f90aeee6411244f08359
noveni <nico.novello@gmail.com>
2021-11-12 03:50:08 +0100

    update mix config

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,8 +14,22 @@ if (mix.inProduction()) {
   })
 }
 
-mix.block('src/scripts/admin/settings/index.js', 'js/theme-admin-settings.js')
-  .sass('src/styles/admin/theme-settings.scss', 'theme-admin-settings.css');
+mix.js('src/scripts/theme/index.js', 'js/theme.js')
+  .sass('src/styles/theme.scss', 'style.css')
+  .block('src/scripts/editor/index.js', 'js/editor.js')
+  .sass('src/styles/editor.scss', 'editor.css')
+  .sass('src/styles/admin-editor.scss', 'admin-editor.css')
+  .js('src/scripts/admin/index.js', 'js/admin.js')
+  .sass('src/styles/admin.scss', 'admin.css')
+  .block('src/scripts/admin/settings/index.js', 'js/theme-admin-settings.js')
+  .sass('src/styles/admin/theme-settings.scss', 'theme-admin-settings.css')
+  .sass('src/styles/woocommerce.scss', 'woocommerce.css')
+  .options({
+    fileLoaderDirs: {
+      images: 'img',
+      fonts: 'fonts'
+    }
+  });
 
 
 mix.setPublicPath('assets');
@@ -24,7 +38,35 @@ mix.autoload({
     jquery: ['$', 'window.jQuery', 'jQuery'], // more than one
   });
 
+mix.sourceMaps(false, 'source-map');
 
+mix.webpackConfig({
+  plugins: [
+    new StylelintPlugin({
+      context: path.resolve(__dirname, 'src/styles'),
+    }),
+    new FaviconsWebpackPlugin({
+      logo: path.resolve(__dirname, './src/favicon.png'),
+      prefix: mix.inProduction() ? path.join('wp-content/themes', path.basename(__dirname), 'assets/icons') : './icons',
+      outputPath: './icons/',
+      inject: false,
+      favicons: {
+        appName: 'Ecran Noir',
+        appDescription: 'Site Vitrine',
+        developerName: null,
+        developerURL: null, // prevent retrieving from the nearest package.json
+        background: '#1D1D1B',
+        theme_color: '#1D1D1B',
+        icons: {
+          coast: false,
+          appleStartup: false,
+          yandex: false,
+          firefox: false
+        }
+      }
+    })
+  ]
+});
 mix.browserSync({
   host: 'starter.localhost',
   open: 'external',
