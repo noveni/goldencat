66cbcbb0b575431e0de68e3611e6d18d1288a9f5
noveni <nico.novello@gmail.com>
2022-09-22 13:44:59 +0200

    correct theme block

--- src/blocks/picked-post-block/picked-post-block.php
+++ src/blocks/picked-post-block/picked-post-block.php
@@ -1,18 +1,23 @@
 <?php
 
-function goldencat_render_picked_term( $attributes, $content ) {
-    if (!isset($attributes['termId'])) {
+function goldencat_render_picked_post( $attributes, $content ) {
+    if (is_admin()) {
         return;
     }
 
-    $term = get_term( $attributes['termId'], $attributes['taxonomy']);
+    if (!$attributes['postId']) {
+        return;
+    }
+
+    $post = get_post( $attributes['postId']);
 
 
     $style = 'style="';
 
     $style .= '"';
 
-    $class = 'wp-block-ecrannoirtwentyone-picked-term';
+    $class = 'wp-block-ecrannoirtwentyone-picked-post wp-block-group';
+    $thumbnail_size = 'thumbnail';
 
     if ( isset( $attributes['align'] ) ) {
 		$class .= ' align' . $attributes['align'];
@@ -22,46 +27,44 @@ function goldencat_render_picked_term( $attributes, $content ) {
 		$class .= ' theme-anim-' . $attributes['themeApparitionEffect'];
 	}
 
-    $term_link = esc_url( get_term_link( $term ) );
-    $title = $term->name;
-        if ( ! $title ) {
-            $title = __( '(no title)' );
-        }
+    $post_link = esc_url( get_permalink( $post ) );
+    $title = get_the_title( $post );
+    if ( ! $title ) {
+        $title = __( '(no title)' );
+    }
     
-    $term_image = false;
-    if ( $attributes['displayImage'] ) {
-        $image_id = get_term_meta( $term->term_id, 'ecrannoirtwentyone-img', true );
-        if( $image = wp_get_attachment_image_src( $image_id ) ) {
-            $term_image = '<img src="' . $image[0] . '" />';
+    $featured_image = false;
+    if ( $attributes['displayFeaturedImage'] ) {
+        if (has_post_thumbnail( $post )) {
+            $featured_image = get_the_post_thumbnail( $post, $thumbnail_size );
         } else {
-            $term_image = goldencat_get_image_placeholder();
+            $featured_image = goldencat_get_image_placeholder();
         }
     }
+
+    $excerpt = wp_trim_words( get_the_excerpt($post), 30, '&hellip;' );
 
     ob_start(); ?>
     <div class="<?php echo $class; ?>" <?php echo $style; ?>>
         <?php
             
         ?>
-        <article>
-            <?php if ( $term_image ) : ?>
-                <a href="<?php echo $term_link; ?>"><figure>
-                <?php echo $term_image; ?>
-                </figure></a>
-            <?php endif; ?> 
-            <header>
-                <h3 class="post-title h2">
-                    <a href="<?php echo $term_link; ?>"><?php echo $title; ?></a>
-                </h3>
-            </header>
-            <?php if ($attributes['displayDescription']): ?>
-                <?php echo $term->description; ?>
-            <?php endif; ?> 
-            <?php goldencat_block_button(array(
-                'href' => $term_link,
-                'label' => esc_html__('Je découvre', 'goldencat')
-            )); ?>
-        </article>
+        <?php if ( $featured_image ) : ?>
+            <a href="<?php echo $post_link; ?>"><figure>
+            <?php echo $featured_image; ?>
+            </figure></a>
+        <?php endif; ?> 
+        <header>
+            <h3 class="post-title">
+                <a href="<?php echo $post_link; ?>"><?php echo $title; ?></a>
+            </h3>
+        </header>
+        <p><?php echo $excerpt; ?></p>
+        <?php goldencat_block_button(array(
+            'href' => $post_link,
+            'label' => esc_html__('Je découvre', 'goldencat'),
+            'style' => 'goldencat-button-link'
+        )); ?>
     </div>
     <?php
     return ob_get_clean();
@@ -70,37 +73,41 @@ function goldencat_render_picked_term( $attributes, $content ) {
 /**
  * Registers the `core/latest-posts` block on server.
  */
-function goldencat_register_block_picked_term_block() {
+function goldencat_register_block_picked_post_block() {
 	register_block_type(
-		'ecrannoirtwentyone/picked-term',
+		'ecrannoirtwentyone/picked-post',
 		array(
             'attributes' => array(
-                'termId' =>                 array(
+                'postId' =>                 array(
                     'type'      => 'number'
                 ),
-                'taxonomy' =>               array(
+                'postType' =>               array(
                     'type'      => 'string',
-                    'default'   => 'category'
+                    'default'   => 'post'
                 ),
-                'displayDescription'            => array(
-                    'type'      => 'boolean',
-                    'default'   => true,
-                ),
-                'descriptionLength'             => array(
-                    'type'      => 'number',
-                    'default'   => 55,
-                ),
-                'displayImage' =>   array(
+                'displayPostContent' =>     array(
                     'type'      => 'boolean',
                     'default'   => true
                 ),
-                'showPostCounts' =>   array(
+                'excerptLength' =>          array(
+                    'type'      => 'number',
+                    'default'   => 55
+                ),
+                'displayAuthor' =>          array(
                     'type'      => 'boolean',
                     'default'   => false
                 ),
+                'displayPostDate' =>        array(
+                    'type'      => 'boolean',
+                    'default'   => true
+                ),
+                'displayFeaturedImage' =>   array(
+                    'type'      => 'boolean',
+                    'default'   => true
+                ),
             ),
-			'render_callback' => 'goldencat_render_picked_term',
+			'render_callback' => 'goldencat_render_picked_post',
 		)
 	);
 }
-add_action( 'init', 'goldencat_register_block_picked_term_block' );
+add_action( 'init', 'goldencat_register_block_picked_post_block' );
